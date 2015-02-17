b1cf47fa280ff0e58e14d8790ce40d9961c7a1ae
kevin <j84255801912@gmail.com>
2015-02-18 02:27:29 +0800

    First

---
+++ simple_test.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python2.7
+#
+# author    :   kevin
+# date      :   20150217
+# purpose   :   very simple test for diqi
+
+import inspect
+import json
+import random
+import sys
+import time
+
+from subprocess import Popen, PIPE, STDOUT
+from peer_addresses import peer_addresses
+
+class Test(object):
+    mint_amount = 10000
+    color_number = 10000
+    sleep_time = 15
+    license = []
+
+    def error(self, *args):
+        sys.stderr.write(' '.join(map(str, args)) + '\n')
+    def rpc_calls(self, *args):
+        args = [i for i in args]
+        try:
+            p = Popen(["bitcoin-cli", "-gcoin"] + args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+            out = p.communicate(input = "")[0].replace("\n", "")
+        except Exception, e:
+            self.error("Error : ", e)
+        return out
+
+    def import_wallet_address(self):
+        self.wallet_address = json.loads(self.rpc_calls("listwalletaddress"))
+        print "%s : done" % (inspect.stack()[0][3],)
+    def import_peer_address(self):
+        self.peer_address = peer_addresses
+        print "%s : done" % (inspect.stack()[0][3],)
+
+    def mint(self, amount, color):
+        return self.rpc_calls("mint", str(amount), str(color))
+    def distribute_money(self, amount, color):
+        for peer in self.peer_address:
+            tx_hash = self.rpc_calls("sendtoaddress", peer, str(amount), str(color))
+    def alliance_track(self):
+        # pre_mint a lot for efficiency
+        mint_tx_hash = self.mint(1, 0)
+
+        # wait for a little time for the mint tx's confirmation
+        print "%s : sleep %d sec" % (inspect.stack()[0][3], self.sleep_time)
+        time.sleep(self.sleep_time)
+
+        # randomly send license
+        peer = random.choice(self.peer_address)
+        color = random.randint(1, self.color_number + 1)
+        tx_hash = self.rpc_calls("sendlicensetoaddress", peer, str(color))
+        print "%s : send %s license to %s" % (inspect.stack()[0][3], str(color), peer)
+    def issuer_track(self):
+        out = json.loads(self.rpc_calls("getlicenseinfo"))
+        all_license = out.keys()
+
+        # mint money that we can mint
+        for i in all_license:
+            mint_tx_hash = self.mint(self.mint_amount, i)
+        if len(all_license) != 0:
+            print "%s : mint money, amount = %f, color = %s" % (inspect.stack()[0][3], self.mint_amount, ' '.join(map(str, all_license)))
+
+        # wait for a little time for the mint tx's confirmation
+        if len(all_license) != 0:
+            print "%s : sleep %d sec" % (inspect.stack()[0][3], self.sleep_time)
+            time.sleep(self.sleep_time)
+
+        # activate all peers
+        for i in all_license:
+            if i not in self.license:
+                self.license.append(i)
+                self.distribute_money(1, i)
+        if len(all_license) != 0:
+            print "%s : activate peers" % (inspect.stack()[0][3],)
+
+    def normal_track(self):
+        # get all available balance
+        out = json.loads(self.rpc_calls("getbalance"))
+
+        # send random color and random amount money randomly
+        for color, money in out.items():
+            if int(color) != 0:
+                for peer in self.peer_address:
+                    if random.choice([True, False]):
+                        # 5 is the magic number
+                        tx_hash = self.rpc_calls("sendtoaddress", peer, str(random.uniform(0, money) / float(5)), str(color))
+        print "%s : send money to the others" % (inspect.stack()[0][3],)
+    def __init__(self):
+        self.import_wallet_address()
+        self.import_peer_address()
+        self._poll()
+    def _poll(self):
+        while True:
+            if __debug__:
+                self.alliance_track()
+            else:
+                self.issuer_track()
+            self.normal_track()
+if __name__ == "__main__":
+    t = Test()
