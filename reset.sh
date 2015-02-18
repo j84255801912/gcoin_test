743dbff9599768440850d06bb01d947d277b155f
kevin <j84255801912@gmail.com>
2015-02-18 14:53:20 +0800

    Some typo

--- reset.sh
+++ reset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # usage     :   e.g.  ./reset.sh 201
-bitcoin-cli -gcoin stop
+killall bitcoind
 mv $HOME/.bitcoin/gcoin $HOME/.bitcoin/gcoin_`date +%s`
 bitcoind -gcoin -daemon
-bitcoin-cli -gcoin stop
+sleep 2
+killall bitcoind
 cp ./wallet.dat.$1 $HOME/.bitcoin/gcoin/wallet.dat
 bitcoind -gcoin -daemon
