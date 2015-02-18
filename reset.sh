c02ff8163b7948730b3cc6ed89e283e22724955e
kevin <j84255801912@gmail.com>
2015-02-18 15:06:13 +0800

    QQ

--- reset.sh
+++ reset.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # usage     :   e.g.  ./reset.sh 201
 killall bitcoind
-mv $HOME/.bitcoin/gcoin $HOME/.bitcoin/gcoin_`date +%Y%m%d%H%M`
+mv $HOME/.bitcoin/gcoin $HOME/.bitcoin/gcoin_`date +%Y%m%d%H%M%S`
+sleep 3
 bitcoind -gcoin -daemon
-sleep 2
+sleep 3
 killall bitcoind
+sleep 3
 cp ./wallet.dat.$1 $HOME/.bitcoin/gcoin/wallet.dat
 bitcoind -gcoin -daemon
