53924dd57f741033c47e796b56bca1aa36afeab4
MaiHsuanChia <j84255801912@gmail.com>
2015-04-08 13:03:15 +0800

    Add edge test nonmember_transactions

--- simple_test.py
+++ simple_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
 #
 # author    :   kevin
 # date      :   20150217
@@ -14,6 +15,8 @@ import time
 from subprocess import Popen, PIPE, STDOUT, call
 from peer_addresses import peer_addresses
 
+EXIT_FAILURE = 1
+
 RPC_SUCCESS = 0
 RPC_ERROR_INVALID_COLOR = 3
 RPC_ERROR_WALLET = 4
@@ -26,6 +29,7 @@ SLEEP_TIME = 15
 MATURITY = 11
 
 def error(*args):
+
     sys.stderr.write(' '.join(map(str, args)) + '\n')
 
 def rpc_calls(*args, **kwargs):
@@ -55,18 +59,22 @@ class Test(object):
     license = []
 
     def import_wallet_address(self):
+
         (code, out) = rpc_calls("listwalletaddress")
         self.wallet_address = json.loads(out)
         print "%s : done" % (inspect.stack()[0][3],)
 
     def import_peer_address(self):
+
         self.peer_address = peer_addresses
         print "%s : done" % (inspect.stack()[0][3],)
 
     def mint(self, amount, color):
+
         return rpc_calls("mint", amount, color)[1]
 
     def activate(self, color):
+
         for i in xrange(len(self.peer_address)):
             self.mint(1, color)
 
@@ -76,6 +84,7 @@ class Test(object):
             (code, tx_hash) = rpc_calls("sendtoaddress", peer, 1, color)
 
     def alliance_track(self):
+
         # pre_mint a lot for efficiency
         mint_tx_hash = self.mint(1, 0)
 
@@ -91,6 +100,7 @@ class Test(object):
                                               str(color), peer)
 
     def issuer_track(self):
+
         (code, out) = rpc_calls("getlicenseinfo")
         out = json.loads(out)
         all_license = out.keys()
@@ -120,6 +130,7 @@ class Test(object):
             time.sleep(SLEEP_TIME)
 
     def normal_track(self):
+
         (code, out) = rpc_calls("getbalance")
         out = json.loads(out)
 
@@ -139,6 +150,7 @@ class Test(object):
                                                   peer)
 
     def reset_bitcoind(self):
+
         return_code = call(["killall", "-9", "bitcoind"])
 
         time.sleep(3)
@@ -152,8 +164,9 @@ class Test(object):
             return False
 
     def reset_and_be_alliance(self, num_trial=10):
+
         if not self.reset_bitcoind():
-            error("Error : reset bitcoind failed\n")
+            error("Error : reset bitcoind failed")
             return False
 
         count = 0
@@ -168,6 +181,7 @@ class Test(object):
         return False
 
     def have_license(self, color):
+
         (code, out) = rpc_calls("getlicenseinfo")
         out = json.loads(out)
         all_license = out.keys()
@@ -205,6 +219,7 @@ class Test(object):
         return flag_complete
 
     def is_alliance(self):
+
         (code, out) = rpc_calls("getmemberlist")
         out = json.loads(out)
         for i in out[u'member_list']:
@@ -232,58 +247,107 @@ class Test(object):
 
         return False
 
+    def get_one_zeroes(self, number):
+
+        for i in xrange(number):
+            code, out = rpc_calls("mint", 1, 0)
+            if code != RPC_SUCCESS:
+                error("Error :", "mint 1 0 failed")
+                sys.exit(EXIT_FAILURE)
+
+        flag_mint_confirmed = self.wait_for_tx_confirmation(out, True)
+        if flag_mint_confirmed != True:
+            error("Error :", "mint 1 0 not confirmed")
+            sys.exit(EXIT_FAILURE)
+
+    def get_license_and_mint(self, address, amount, color):
+
+        self.get_one_zeroes(1)
+
+        code, out = rpc_calls("sendlicensetoaddress", address, color)
+        if code != RPC_SUCCESS:
+            error("Error :", "sendlicensetoaddress failed")
+            sys.exit(EXIT_FAILURE)
+        flag_license_confirmed = self.wait_for_tx_confirmation(out)
+        if flag_license_confirmed != True:
+            error("Error :", "license not confirmed")
+            sys.exit(EXIT_FAILURE)
+
+        code, out = rpc_calls("mint", amount, color)
+        if code != RPC_SUCCESS:
+            error("Error :", "mint {0} {1} failed".format(amount, color))
+            sys.exit(EXIT_FAILURE)
+        flag_mint_confirmed = self.wait_for_tx_confirmation(out, True)
+        if flag_mint_confirmed != True:
+            error("Error :", "mint {0} {1} not confirmed".format(amount,
+                                                                 color))
+            sys.exit(EXIT_FAILURE)
+
     def color_test(self, color):
         """ Return if the license transaction is valid
 
             return value:
                 True,  if color is ok.
                 False, if color is not ok.
-                None,  if there is unexpected error.
         """
 
-        if self.is_alliance() != True:
-            error("Error: ", "You are not an alliance.")
-            return None
-
         (code, txid) = rpc_calls("mint", 1, 0)
-        print "Waiting for mint tx: {0} to be confirmed...".format(txid)
 
         flag_mint_confirmed = self.wait_for_tx_confirmation(txid, True)
-        if not flag_mint_confirmed:
-            return None
+        if flag_mint_confirmed != True:
+            error("Error :", "mint 1 0 not confirmed")
+            sys.exit(EXIT_FAILURE)
 
         (code, txid) = rpc_calls("sendlicensetoaddress",
                                  self.wallet_address[0], color)
         if code == RPC_ERROR_INVALID_COLOR:
             return False
         elif code != RPC_ERROR_INVALID_COLOR and code != RPC_SUCCESS:
-            return None
+            error("Error :", "sendlicensetoaddress failed")
+            sys.exit(EXIT_FAILURE)
 
 #        print "Waiting for license tx: {0} to be confirmed...".format(txid)
         flag_license_confirmed = self.wait_for_tx_confirmation(txid)
+        if flag_license_confirmed != True:
+            error("Error :", "license not confirmed")
+            sys.exit(EXIT_FAILURE)
 
-        return None if flag_license_confirmed == False else True
+        return True
 
     def usable_color_test(self):
         """ edge test 3-1 """
 
         flag_complete = True
-        testing_color = [UINT32_MIN - 1, UINT32_MIN,
-                         UINT32_MIN + 1, UINT32_MAX - 1,
-                         UINT32_MAX, UINT32_MAX + 1]
+        testing_color = [UINT32_MIN - 1, UINT32_MIN, UINT32_MIN + 1,
+                         UINT32_MAX - 1, UINT32_MAX, UINT32_MAX + 1]
         testing_answer = [False, None, True, True, True, False]
 
         for i in xrange(len(testing_color)):
             result = self.color_test(testing_color[i])
             if result != testing_answer[i]:
-                error("Error :", "color %d failed, result=%r answer=%r"
-                                    % (testing_color[i], result,
-                                       testing_answer[i]))
+                print "color %d failed, result=%r answer=%r" % (
+                       testing_color[i], result, testing_answer[i])
                 flag_complete = False
 
         return flag_complete
 
-    def mint_amount(self):
+    def mint_test(self, amount, color):
+
+        if self.color_test(color) != True:
+            error("Error :", "something wrong in creating license")
+            sys.exit(EXIT_FAILURE)
+
+        code, out = rpc_calls("mint", amount, color)
+        if code == RPC_SUCCESS:
+            result = True
+        elif code == RPC_ERROR_WALLET:
+            result = False
+        else:
+            error("Error: ", "mint not confirmed")
+            sys.exit(EXIT_FAILURE)
+        return result
+
+    def mint_amount_test(self):
         """ edge test 3-2
 
             [Caution]
@@ -291,39 +355,60 @@ class Test(object):
             2. Max amount is 10**10
             3. Using multiple colors to prevent from overflow of coins of
                one single color
-        """
-        MAX_AMOUNT = 10 ** 10
 
+            [Return Value]
+                True    :   pass
+                False   :   fail
+        """
+
+        MAX_AMOUNT = 10 ** 10
         flag_complete = True
-        testing_color = [1, 2, 3, 4, 5, 6]
         testing_amount = [-1, 0, 1, MAX_AMOUNT - 1, MAX_AMOUNT, MAX_AMOUNT + 1]
+        testing_color = [1, 2, 3, 4, 5, 6]
         testing_answer = [False, False, True, True, True, False]
 
         # preparing for licenses
 
         for i in xrange(len(testing_amount)):
-            if self.color_test(testing_color[i]) != True:
-                error("Error :", "something wrong in creating license")
-                return None
-            code, out = rpc_calls("mint", testing_amount[i], testing_color[i])
-
-            if code == RPC_SUCCESS:
-                result = True
-            elif code == RPC_ERROR_WALLET:
-                result = False
-            else:
-                result = None
-
+            result = self.mint_test(testing_amount[i], testing_color[i])
             if result != testing_answer[i]:
-                error("Error :", "testing_amount %d failed" %
-                                 testing_amount[i])
+                print "Error :", "testing_amount %d failed" % testing_amount[i]
                 flag_complete = False
         return flag_complete
 
+    def nonmember_transactions(self):
+        """ edge test 1-3
+
+            [Return Value]
+                True    :   pass
+                False   :   fail
+        """
+
+        self.get_license_and_mint(self.wallet_address[0], 1, 123)
+
+        # activate wallet_address[1]
+        code, out = rpc_calls("sendtoaddress", self.wallet_address[1], 1, 123)
+        if code != RPC_SUCCESS:
+            error("Error :", "sendtoaddress failed")
+            sys.exit(EXIT_FAILURE)
+        flag_tx_confirmed = self.wait_for_tx_confirmation(out)
+        if flag_tx_confirmed != True:
+            error("Error :", "sendtoaddress tx not confirmed")
+            sys.exit(EXIT_FAILURE)
+
+        # transfer money to a nonmember, i.e. wallet_address[2]
+        code, out = rpc_calls("sendtoaddress", self.wallet_address[2], 1, 123)
+        if code != RPC_ERROR_WALLET:
+            return False
+        return True
+
     def __init__(self):
+
         self.reset_and_be_alliance()
         self.import_wallet_address()
-        print self.mint_amount()
+        print self.nonmember_transactions()
+#        print self.mint_amount_test()
+#        self.get_license_and_mint(self.wallet_address[0], 10000, 123)
 #        print self.usable_color_test()
 #        print self.is_alliance()
 #        print self.have_license(10)
@@ -332,6 +417,7 @@ class Test(object):
 #        self._auto_run()
 
     def _auto_run(self):
+
         while True:
             if __debug__:
                 self.alliance_track()
